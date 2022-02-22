78af6c8ca321b11db6634c592662128d1ba4a38f
igorpex <igor.bogdanov@gmail.com>
2022-02-22 12:29:55 +0300

    feat: reverse number done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = ''
+    let str = Math.abs(n).toString();
+    for (symbol of str) {
+        result = symbol + result;
+    }
+    return Number(result);
 }
